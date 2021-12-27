8fdb232d165fe2f5e1128e7a0de55faff3fa14ed
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-12-27 20:45:19 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# calculator-Bootrsap
+
+# see deployed [link](https://optimistic-bardeen-4b9a9f.netlify.app/)
+
+calculator designed using JavaScript and CSS,HTML , getting data while pressing keys, then get output in display by eval( ) function.
---
+++ calculatorBoots.js
@@ -0,0 +1,26 @@
+let ans = document.querySelector("#result");
+
+function conCat(keys) {
+  ans.value += keys; //concat the two numbers
+  console.log(ans.value);
+}
+
+function result() {
+  if (ans.value == "") {
+    confirm("Enter Number First...!");
+    ans.value = 0; //reseting values
+  }
+  try {
+    ans.value = eval(ans.value); //arithmatic operations:
+  } catch (err) {
+    alert("Enter Valid Input");
+    ans.value = 0;
+  }
+}
+function del() {
+  ans.value = 0;
+}
+
+function backSpace() {
+  ans.value = ans.value.slice(0, -1);
+}
